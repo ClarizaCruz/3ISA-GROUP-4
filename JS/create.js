fc8b901c19f885d7df279cad8e0250639196345e
ClarizaCruz <hansroy.billiones.iics@ust.edu.ph>
2021-09-09 08:07:17 +0800

    initial prototype

---
+++ JS/create.js
@@ -0,0 +1,7 @@
+function cancelbutton(){
+    alert(document.getElementById("cancel").value)
+}
+
+function savebutton(){
+    alert(document.getElementById("save").value)
+}
---
+++ JS/mode.js
@@ -0,0 +1,4 @@
+function changeMode(){
+    document.body.style.backgroundColor = "#fffbfb";
+    this.document.getElementByClassName("Dark-mode-body").style.backgroundColor = "#fffbfb";
+    }
