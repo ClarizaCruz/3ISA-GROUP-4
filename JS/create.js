1401f0b4111ca46a127c07acc26e6adf3a370ee2
ClarizaCruz <hansroy.billiones.iics@ust.edu.ph>
2021-09-28 08:53:06 +0800

    Updated create daymode

--- JS/create.js
+++ JS/create.js
@@ -5,3 +5,16 @@ function cancelbutton(){
 function savebutton(){
     alert(document.getElementById("save").value)
 }
+
+function vat(totalsales){
+    this.totalsales = document.getElementById("totalSales").value;
+    
+
+var input = document.getElementById("totalSales");
+input.addEventListener("keyup", function(event) {
+  if (KeyboardEvent.keyCode === 13) {
+    document.getElementById("vat").innerHTML = this.totalsales*.12;
+   event.preventDefault();
+   document.getElementById("vat").click();
+   
+  }})}
