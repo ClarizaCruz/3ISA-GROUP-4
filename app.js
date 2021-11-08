5424cc626d2409d5524e60105544d3b2762f75a7
ClarizaCruz <70572090+ClarizaCruz@users.noreply.github.com>
2021-11-09 04:24:53 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 //imports
 const express =  require('express')
-const app = express()
+const mysql = require('mysql');
 
 const port = process.env.PORT || 3000
+const app = express();
+
+
+//connect the node app with MySql server
+const con = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Clariza0421!",
+});
+
+con.connect((err) => {
+  if (!err){
+      console.log("connected to MySql server at port 3306...");
+  }
+});
+
 
 //static files
 app.use(express.static('public'))
