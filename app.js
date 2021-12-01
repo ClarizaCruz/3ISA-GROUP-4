6e12b9e3303a1b546afce0da44dcf53216ed0c91
haansbillones <hansroy.billones.iics@ust.edu.ph>
2021-12-02 03:00:01 +0800

    added login backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //imports
 const express =  require('express');
 const exphbs = require('express-handlebars');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const mysql = require('mysql');
+const encoder = bodyParser.urlencoded();
 
 
 require('dotenv').config();
@@ -11,6 +12,14 @@ require('dotenv').config();
 //dotenv.config({path: './.env'});
 const port = process.env.PORT || 3000
 const app = express();
+
+//database connection
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
 
 //Static Files
 const publicDirectory = path.join(__dirname, './public' );
@@ -41,8 +50,36 @@ app.use('/', routes);
 const VD = require('./server/routes/viewDay');
 app.use('/', VD);
 
+//login function
+connection.connect((error)=>{
+    if (error) throw error
+    else console.log("connected to the database successfully!")
+});
+
+
+app.get("/",function(req,res){
+    res.render("index");
+})
+
+app.post("/",encoder,(req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("SELECT * FROM login WHERE user_name = ? AND user_pass = ?",[username,password],(error,results,fields) => {
+        if (results.length > 0) {
+            res.redirect("/homepage");
+        } else {
+            res.redirect("/");
+        }
+        res.end();
+    })
+})
+
+// when login is success
+app.get("/homepage",(req,res)=>{
+    res.render("homepage")
+})
+
+
 //listen on port 3000
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
